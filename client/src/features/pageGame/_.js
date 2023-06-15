09b291bd8c41a2f224141679d8d995edf380ed9d
zdllucky <animereader@yandex.kz>
2023-06-16 01:14:48 +0600

    Implemented player progress refresh

--- client/src/features/pageGame/_.js
+++ client/src/features/pageGame/_.js
@@ -1,5 +1,6 @@
 import Alpine from "alpinejs";
 import { getUserCards } from "./repoCards.js";
+import { getGamePlayerProgress } from "./repoGame.js";
 
 Alpine.data("gameCards", () => ({
   cards: undefined,
@@ -14,3 +15,33 @@ Alpine.data("gameCards", () => ({
     }
   },
 }));
+
+Alpine.data("gamePlayerProgress", () => ({
+  playersProgress: [],
+  refreshIntervalId: undefined,
+  init() {
+    this.loadProgress();
+
+    this.refreshIntervalId = setInterval(() => this.loadProgress(), 5000);
+  },
+  destroy() {
+    clearInterval(this.refreshIntervalId);
+  },
+  async loadProgress() {
+    try {
+      this.playersProgress = chunkArray(await getGamePlayerProgress(), 2);
+    } catch (e) {
+      this.playersProgress = [];
+    }
+  },
+}));
+
+function chunkArray(array, chunkSize) {
+  let results = [];
+
+  for (let i = 0; i < array.length; i += chunkSize) {
+    results.push(array.slice(i, i + chunkSize));
+  }
+
+  return results;
+}
---
+++ client/src/features/pageGame/repoGame.js
@@ -0,0 +1,21 @@
+import Alpine from "alpinejs";
+export const getGamePlayerProgress = async () => {
+  const res = await fetch("http://localhost:3000/api/graphql", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      Authorization: "Bearer " + Alpine.store("auth").token,
+    },
+    body: JSON.stringify({
+      query: `query GamePlayerProgress {
+        gamePlayerProgress {
+          id
+          login
+          progress
+        }
+      }`,
+    }),
+  });
+
+  return (await res.json()).data.gamePlayerProgress;
+};
