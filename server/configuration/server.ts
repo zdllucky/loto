21c537743971861200896ac8fb25456e646e2204
zdllucky <animereader@yandex.kz>
2023-06-27 22:39:03 +0600

    Added proper cors

--- server/configuration/server.ts
+++ server/configuration/server.ts
@@ -1,13 +1,21 @@
 import extendExpressApp from "../rest";
 import { Config } from ".keystone/types";
 
+const corsOptions = {
+  origin:
+    process.env.NODE_ENV === "production"
+      ? process.env.FRONTEND_URL
+      : "http://localhost:3333",
+  optionsSuccessStatus: 200,
+};
+
 const server: Config["server"] = {
   extendExpressApp,
   cors: {
     preflightContinue: false,
     methods: ["GET", "POST"],
     credentials: false,
-    origin: "*",
+    ...corsOptions,
   },
 };
 
