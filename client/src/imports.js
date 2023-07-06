63c68e0afd1fce2330ae556f2bd2a4f2cdf93ac7
zdllucky <animereader@yandex.kz>
2023-07-07 01:07:06 +0600

    Added animation to lists and balls;

--- client/src/imports.js
+++ client/src/imports.js
@@ -4,10 +4,14 @@ import router from "@shaun/alpinejs-router";
 import formData from "alpinejs-form-data";
 import "bootstrap-icons/font/bootstrap-icons.scss";
 import "./imports.sass";
+import autoAnimate from "@formkit/auto-animate";
 
 Object.defineProperty(window, "Alpine", {
   writable: false,
   value: Alpine,
+});
+Alpine.directive("autoanimate", (el, { expression }, { evaluate }) => {
+  autoAnimate(el, (expression && evaluate(expression)) || undefined);
 });
 
 onAlpineInit(({ plugin }) => {
